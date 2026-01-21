ab2a4fd06406abf42bafe81d5d2c6e6b4e6888f9
TranDinh <trandtb@greenglobal.vn>
2026-01-21 22:40:05 +0700

    Cập nhật config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: "smartcom", // Tên ứng dụng của bạn trong pm2
-      script: "yarn", // Lệnh để chạy
-      args: "start", // Tham số truyền cho lệnh `script` (tương đương `yarn start`)
+      script: "npm", // Lệnh để chạy
+      args: "run start", // Tham số truyền cho lệnh `script` (tương đương `yarn start`)
       interpreter: "bash", // Chỉ định rõ trình thông dịch là bash
       exec_mode: "fork", // Chế độ thực thi phù hợp cho yarn/npm
       env: {
